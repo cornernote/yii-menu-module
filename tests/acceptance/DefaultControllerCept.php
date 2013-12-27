86e6e2b212e41f6ebd8243ce53948c9e0f88d67e
cornernote <cornernote@gmail.com>
2013-12-27 14:04:16 +1030

    travis

--- tests/acceptance/DefaultControllerCept.php
+++ tests/acceptance/DefaultControllerCept.php
@@ -7,29 +7,14 @@
  * @author Brett O'Donnell <cornernote@gmail.com>
  * @author Zain Ul abidin <zainengineer@gmail.com>
  * @copyright 2013 Mr PHP
- * @link https://github.com/cornernote/yii-email-module
- * @license BSD-3-Clause https://raw.github.com/cornernote/yii-email-module/master/LICENSE
+ * @link https://github.com/cornernote/yii-menu-module
+ * @license BSD-3-Clause https://raw.github.com/cornernote/yii-menu-module/master/LICENSE
  *
- * @package yii-email-module
+ * @package yii-menu-module
  */
 
 $I = new WebGuy($scenario);
 $I->wantTo('ensure DefaultController works');
 
-$I->amOnPage('email');
-$I->see('You may use the following tools');
-
-$I->click('Error');
-$I->see('Errors');
-
-$I->click('Field');
-$I->see('Fields');
-
-$I->click('Log');
-$I->see('Logs');
-
-$I->click('Request');
-$I->see('Requests');
-
-$I->click('Email');
+$I->amOnPage('menu');
 $I->see('You may use the following tools');
